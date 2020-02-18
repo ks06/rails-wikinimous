fc7b31277b38ad32bdac46d371826484a576f520
Kate Shin <kzzzsy111@gmail.com>
2020-02-18 22:16:19 +0000

    Added index, create, new and show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,38 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, except: %i[index new]
+
   def index
+    @articles = Article.all
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
   end
 
-  def show
-  end
+  def show; end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
